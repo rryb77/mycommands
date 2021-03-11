3d301fc18ba5a04378cf13592ca2c968ce12784f
Ryan Youngblood <rryb77@gmail.com>
2021-03-11 09:51:51 -0600

    initial commit

---
+++ MyHead/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+const int LINE_COUNT = 10;
+
+if (args.Length != 1)
+{
+    Console.Error.WriteLine("USAGE: myhead <filename>");
+    Environment.Exit(1);
+}
+
+var filename = args[0];
+
+if (!File.Exists(filename))
+{
+    Console.Error.WriteLine($"No such file: {filename}");
+    Environment.Exit(1);
+}
+
+using var file = new StreamReader(filename);
+for (var i = 0; i < LINE_COUNT; i++)
+{
+    var line = file.ReadLine();
+    if (line == null)
+    {
+        return;
+    }
+    Console.WriteLine(line);
+}
