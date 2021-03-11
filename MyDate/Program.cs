6e572370a6ceec0c2aca8b293992b0136ce09264
Ryan Youngblood <rryb77@gmail.com>
2021-03-11 14:01:25 -0600

    mydate is now completed

---
+++ MyDate/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyDate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            if (args.Length != 1)
+            {
+                Console.Error.WriteLine("USAGE: mydate");
+                Environment.Exit(1);
+            }
+
+            DayOfWeek dayOfWeek = DateTime.Today.DayOfWeek;
+            DateTime month = DateTime.Now;
+            int day = DateTime.Today.Day;
+            string time = DateTime.Now.ToString("h:mm:ss tt");
+            TimeZoneInfo localZone = TimeZoneInfo.Local;
+            int year = DateTime.Now.Year;
+            Console.WriteLine($"{dayOfWeek} {month.ToString("MMM")}  {day} {time} {localZone.Id} {year}");
+        }
+    }
+}
