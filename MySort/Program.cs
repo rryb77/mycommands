176144158727ef6c5aab17ae51a3d273ffcb10d6
Ryan Youngblood <rryb77@gmail.com>
2021-03-11 11:55:58 -0600

    mysort command is complete

---
+++ MySort/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MySort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.Error.WriteLine("USAGE: mysort <filename>");
+                Environment.Exit(1);
+            }
+
+            var filename = args[0];
+
+            if (!File.Exists(filename))
+            {
+                Console.Error.WriteLine($"No such file: {filename}");
+                Environment.Exit(1);
+            }
+
+            using var file = new StreamReader(filename);
+            List<string> listOfLines = new List<string>();
+
+            var reader = new StreamReader(filename);
+
+            while (reader.ReadLine() != null)
+            {
+                string line = file.ReadLine();
+                listOfLines.Add(line);
+            }
+
+            listOfLines.Sort();
+
+            foreach (string line in listOfLines)
+            {
+                Console.WriteLine(line);
+            }
+            
+        }
+    }
+}
