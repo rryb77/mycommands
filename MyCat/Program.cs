a3301f0745b52566b77c73528e8920251d6b99a7
Ryan Youngblood <rryb77@gmail.com>
2021-03-11 11:28:49 -0600

    mycat is now functioning

---
+++ MyCat/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MyCat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.Error.WriteLine("USAGE: mycat <filename>");
+                Environment.Exit(1);
+            }
+
+            var filename = args[0];
+
+            if (!File.Exists(filename))
+            {
+                Console.Error.WriteLine($"No such file: {filename}");
+                Environment.Exit(1);
+            }
+
+            using var file = new StreamReader(filename);
+            List<string> listOfLines = new List<string>();
+
+            var reader = new StreamReader(filename);
+
+            while (reader.ReadLine() != null)
+            {
+                string line = file.ReadLine();
+                listOfLines.Add(line);
+            }
+
+            foreach (string line in listOfLines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
