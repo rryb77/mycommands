d1fa0f8c10d2cf1b92dd2430f7e93e3c48c65436
Ryan Youngblood <rryb77@gmail.com>
2021-03-11 14:52:06 -0600

    myls is now complete

---
+++ MyLs/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace MyLs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "";
+
+            if (args.Length < 2)
+            {
+                path = Directory.GetCurrentDirectory();
+            }
+            else
+            {
+                path = args[1];
+            }
+
+            string[] theFiles = Directory.GetFiles(path);
+            string[] theDirectories = Directory.GetDirectories(path);
+
+            foreach (string dir in theDirectories)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                string dirNameOnly = dir.Replace(path + @"\", "");
+                Console.WriteLine(dirNameOnly + "/");
+            }
+
+            foreach (string file in theFiles)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                string fileNameOnly = file.Replace(path + @"\", "");
+                Console.WriteLine(fileNameOnly);
+            }
+        }
+    }
+}
