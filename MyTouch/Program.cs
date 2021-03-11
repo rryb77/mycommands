18ecf8dcbe89251adf60d944e59e8feb47558e89
Ryan Youngblood <rryb77@gmail.com>
2021-03-11 13:14:07 -0600

    mytouch command is complete

---
+++ MyTouch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace MyTouch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var filename = args[0];
+
+            if (args.Length != 1)
+            {
+                Console.Error.WriteLine("USAGE: mytouch <filename>");
+                Environment.Exit(1);
+            }
+
+            if (File.Exists(filename))
+            {
+                Console.WriteLine($"{filename} already exists, would you like to overwrite it? [Y/N]");
+                string answer = Console.ReadLine().ToLower();
+
+                if (answer == "y")
+                {
+                    File.Create($"{filename}");
+                }
+                else
+                {
+                    Environment.Exit(1);
+                }
+            }
+            else
+            {
+                File.Create($"{filename}");
+            }
+        }
+    }
+}
